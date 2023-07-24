a0c5063efcc8e199682e1ada3ffef238b395a0a2
immanuel <immanuelajimah@gmail.com>
2023-07-24 04:46:21 +0100

    created state configured react-persist, mui dark-mode  and app layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
+import Home from './Pages/homePage/Home'
+import Login from './Pages/loginPage/Login'
+import Profile from './Pages/profilePage/Profile'
+import { ThemeProvider, createTheme } from '@mui/material/styles'
+import CssBaseline from '@mui/material/CssBaseline'
+import { useSelector } from 'react-redux'
+import { themeSettings } from './theme'
+import { useMemo } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const mode = useSelector((state) => state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode), [mode]))
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route>
+        <Route index element={<Login />} />
+        <Route path="/hame" element={<Home />} />
+        <Route path="/profile/:userId" element={<Profile />} />
+      </Route>,
+    ),
+  )
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <RouterProvider router={router} />
+    </ThemeProvider>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
+import authReducer from './States/index.js'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PURGE,
+  PERSIST,
+  PAUSE,
+  REGISTER,
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { PersistGate } from 'redux-persist/integration/react'
 
+const persistConfig = { key: 'root', storage, version: 1 }
+const persistedReducer = persistReducer(persistConfig, authReducer)
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PERSIST, PURGE, PAUSE, REGISTER],
+      },
+    }),
+})
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 )
